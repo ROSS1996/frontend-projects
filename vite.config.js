005b131ce2d3c5c8ba643f7675e40aeeb88d1fa3
ROSS1996 <41270745+ROSS1996@users.noreply.github.com>
2023-01-24 16:43:48 -0300

    Update design for desktop

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePluginFonts } from 'vite-plugin-fonts'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePluginFonts({
+      google: {
+        families: ['Century Gothic'],
+      },
+    }),
+  ],
   base: './',
 })
