d1b313f182b8bd43741aa6bdbd70942217de703d
ROSS1996 <41270745+ROSS1996@users.noreply.github.com>
2023-01-24 22:04:44 -0300

    Add filtering

--- joblist-int/src/App.jsx
+++ joblist-int/src/App.jsx
@@ -13,68 +13,76 @@ function App() {
   )
 
 
-  function updateFilters(value, type) {
-    if (type === 'role') {
-      if (!filters.role.includes(value)) {
-        // Create a new object with the updated array
+  function updateFilters(value, type, remove = false) {
+    if (remove) {
+      if (type === 'all') {
         const updatedFilters = {
-          ...filters,
-          role: [...filters.role, value],
+          role: [],
+          level: [],
+          languages: [],
+          tools: []
         };
-        // Call setFilters with the new object
+        setFilters(updatedFilters);
+      } else {
+        const updatedFilters = { ...filters };
+        updatedFilters[type] = updatedFilters[type].filter(val => val !== value);
         setFilters(updatedFilters);
       }
-    }
-    if (type === 'level') {
-      if (!filters.level.includes(value)) {
-        // Create a new object with the updated array
-        const updatedFilters = {
-          ...filters,
-          level: [...filters.level, value],
-        };
-        // Call setFilters with the new object
-        setFilters(updatedFilters);
+    } else {
+      if (type === 'role') {
+        if (!filters.role.includes(value)) {
+          // Create a new object with the updated array
+          const updatedFilters = {
+            ...filters,
+            role: [...filters.role, value],
+          };
+          // Call setFilters with the new object
+          setFilters(updatedFilters);
+        }
       }
-    }
-    if (type === 'language') {
-      if (!filters.languages.includes(value)) {
-        // Create a new object with the updated array
-        const updatedFilters = {
-          ...filters,
-          languages: [...filters.languages, value],
-        };
-        // Call setFilters with the new object
-        setFilters(updatedFilters);
+      if (type === 'level') {
+        if (!filters.level.includes(value)) {
+          // Create a new object with the updated array
+          const updatedFilters = {
+            ...filters,
+            level: [...filters.level, value],
+          };
+          // Call setFilters with the new object
+          setFilters(updatedFilters);
+        }
       }
-    }
-    if (type === 'tool') {
-      if (!filters.tools.includes(value)) {
-        // Create a new object with the updated array
-        const updatedFilters = {
-          ...filters,
-          tools: [...filters.tools, value],
-        };
-        // Call setFilters with the new object
-        setFilters(updatedFilters);
+      if (type === 'language') {
+        if (!filters.languages.includes(value)) {
+          // Create a new object with the updated array
+          const updatedFilters = {
+            ...filters,
+            languages: [...filters.languages, value],
+          };
+          // Call setFilters with the new object
+          setFilters(updatedFilters);
+        }
       }
-    }
-    if (type === 'clear') {
-      const updatedFilters = {
-        role: [],
-        level: [],
-        languages: [],
-        tools: []
-      };
-      setFilters(updatedFilters);
+      if (type === 'tool') {
+        if (!filters.tools.includes(value)) {
+          // Create a new object with the updated array
+          const updatedFilters = {
+            ...filters,
+            tools: [...filters.tools, value],
+          };
+          // Call setFilters with the new object
+          setFilters(updatedFilters);
+        }
+      }
     }
   }
+
 
   return (
     <div className="h-screen font-leagueSpartan">
       <div className="h-1/5 bg-header-desktop bg-no-repeat bg-cover bg-desaturatedDark">
         < Filter filters={filters} updateFilters={updateFilters} />
       </div>
-      < Listings updateFilters={updateFilters} />
+      < Listings filters={filters} updateFilters={updateFilters} />
     </div>
   )
 }
