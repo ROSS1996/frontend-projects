41cac0c660156fa39e16c22621993b56d875add2
ROSS1996 <41270745+ROSS1996@users.noreply.github.com>
2023-01-25 18:05:35 -0300

    Add styles to page

--- linkshort-int/src/components/header.jsx
+++ linkshort-int/src/components/header.jsx
@@ -1,14 +1,21 @@
 import styles from "../assets/styles/Header.module.css";
 
+import logo from "../assets/logo.svg";
+
 export default function Header() {
   return (
     <header>
-      <div>
-        <span>Features</span> <span>Pricing</span> <span>Resources</span>
-      </div>
-      <div>
-        <button>Login</button>
-        <button>Sign Up</button>
+      <img src={logo} alt="Logo" className={styles.logo} />
+      <div className={styles.options}>
+        <ul className={styles.linksList}>
+          <span>Features</span>
+          <span>Pricing</span>
+          <span>Resources</span>
+        </ul>
+        <div className={styles.account}>
+          <button className={styles.login}>Login</button>
+          <button className={styles.signUp}>Sign Up</button>
+        </div>
       </div>
     </header>
   );
