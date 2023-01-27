18249cf9c4f7bd58a1f67eac5299dacd560dad6f
ROSS1996 <41270745+ROSS1996@users.noreply.github.com>
2023-01-27 18:47:58 -0300

    Add more media queries

--- linkshort-int/src/components/layout.jsx
+++ linkshort-int/src/components/layout.jsx
@@ -1,12 +1,34 @@
+import { useState, useEffect } from "react";
+
 import Footer from "./footer";
 import Header from "./header";
 
 export default function Layout({ children }) {
+  function getWindowDimensions() {
+    const { innerWidth: width, innerHeight: height } = window;
+    return {
+      width,
+      height,
+    };
+  }
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions()
+  );
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <>
-      <Header />
+      <Header windowDimensions={windowDimensions} />
       <main>{children}</main>
-      <Footer />
+      <Footer windowDimensions={windowDimensions} />
     </>
   );
 }
